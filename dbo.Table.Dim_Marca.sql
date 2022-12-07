1b15feb9afba828282c9fb9f8b88493a0688bc40
Leonardo <leonarddo.gomees@gmail.com>
2022-12-07 20:46:46 -0300

    Adicionando tabelas no DB

---
+++ dbo.Table.Dim_Marca.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].Dim_Marca
+(
+	[Cod_Marca] NCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_Marca] NCHAR(200) NULL, 
+    [Cod_Categoria] NCHAR(50) NULL, 
+    CONSTRAINT [FK_Dim_Marca_Dim_Categoria] FOREIGN KEY ([Cod_Categoria]) REFERENCES [Dim_Categoria]([Cod_Categoria])
+)
---
+++ dbo.Table.dim_categoria.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].Dim_Categoria
+(
+	[Cod_categoria] NCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_categoria] NCHAR(200) NULL
+)
